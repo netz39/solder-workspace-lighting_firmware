b9b9e53bf76f5584310d88feca9b6b661113c47b
MG-5 <mg-95@t-online.de>
2022-09-28 21:53:43 +0200

    Enable missing pull-up on encoder button

--- cubemx/Core/Src/gpio.c
+++ cubemx/Core/Src/gpio.c
@@ -70,7 +70,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = EncoderButton_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(EncoderButton_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PBPin PBPin PBPin */
